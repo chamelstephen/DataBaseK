40c96f1d23223547bbfe0db6ea082ae71a7b55c5
kento <kentostephenohara@gmail.com>
2015-12-10 12:08:28 +0900

    added classes

--- DataBaseofKingdom/DataTableViewController.swift
+++ DataBaseofKingdom/DataTableViewController.swift
@@ -1,5 +1,5 @@
 //
-//  DataViewController.swift
+//  DataTableViewController.swift
 //  DataBaseofKingdom
 //
 //  Created by kento on 2015/12/10.
@@ -8,82 +8,53 @@
 
 import UIKit
 
-class DataViewController: UIViewController/*, UITableViewDelegate, UITableViewDataSource*/ {
+class DataTableViewController: UITableViewController/*, UITableViewDelegate, UITableViewDataSource*/  {
     
-    /*
     @IBOutlet var dataTableView: UITableView!
     @IBOutlet var dataSegCon: UISegmentedControl!
     @IBOutlet var searchBut: UIButton!
     @IBOutlet var favoriteBut: UIButton!
     
     var dataItems: [String] = ["楊端和", "白起"]
-    
-    */
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
-        dataTableView.delegate = self
-        dataTableView.dataSource = self
+        dataTableView?.delegate = self
+        dataTableView?.dataSource = self
         dataTableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "DataCell")
         
         let dataxib = UINib(nibName: "DataItemsTableViewCell", bundle: nil)
         dataTableView.registerNib(dataxib, forCellReuseIdentifier: "DataCell")
-        */
-
-        // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    // MARK: - Table view data source
     
-    /*
-    /*
-    テーブルに表示するセクションの数を返す.
-    */
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    //sectionの数を返す
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         
-        return 0
-    }
-    
-    /*
-    Cellが選択された際に呼び出される.
-    */
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-    
-    //
+        return 1
         
     }
-    
-    /*
-    テーブルに表示する配列の総数を返す.
-    */
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    //配列の総数を返す
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return dataItems.count
         
     }
-    
-    /*
-    Cellに値を設定する.
-    */
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+    //cellに値を設定
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCellWithIdentifier("DataCell", forIndexPath: indexPath) as! DataItemsTableViewCell
-        
+
         //icView
         
         
@@ -109,12 +80,19 @@ class DataViewController: UIViewController/*, UITableViewDelegate, UITableViewDa
         //defenceLab
         cell.defenceLab?.layer.masksToBounds = true
         cell.defenceLab?.layer.cornerRadius = 3
-        
-        
+
         return cell
         
     }
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
     */
 
 }
